bd27231b58c8e03b4d46880c37fb6b2f740b7319
Miguel <miguelszcorti@gmail.com>
2014-10-06 12:00:54 -0300

    Fixing Show

--- app/models/nominee.rb
+++ app/models/nominee.rb
@@ -1,7 +1,2 @@
 class Nominee < ActiveRecord::Base
-	#has_many: votes
-	#validates :name, presence: true, length: { maximum: 50 }
-	#validates :number, presence: true
-	#validates :photo, presence: true
-
 end
