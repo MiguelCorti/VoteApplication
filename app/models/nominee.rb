78eda58d14fd580d36b6f5fe7f1485103d2482d7
Miguel <miguelszcorti@gmail.com>
2014-10-06 11:09:24 -0300

    Nominees have photos

--- app/models/nominee.rb
+++ app/models/nominee.rb
@@ -1,2 +1,7 @@
 class Nominee < ActiveRecord::Base
+	has_many: votes
+	validates :name, presence: true, length: { maximum: 50 }
+	validates :number, presence: true
+	validates :photo, presence: true
+
 end
